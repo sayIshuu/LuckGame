0e5a4adcb35ea784a7d983bf7e2bfac12dd17b9a
sayIshuu <smile52673@gmail.com>
2024-09-29 04:31:54 +0900

    [fix] GameManager의 캐시데이터 남는 문제 해결
    아예 발생하지 않게 싱글톤구조를 단순화 시켰다.
    씬 복수 발생 시 다시 점검해볼것

--- LuckGame/Assets/Scripts/GameManager.cs
+++ LuckGame/Assets/Scripts/GameManager.cs
@@ -5,9 +5,24 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     private static GameManager _instance;
+    public static GameManager Instance { get { return _instance; } }
 
     private int monsterCount = 0;
 
+    private void Awake()
+    {
+        // 싱글톤 인스턴스를 설정
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    /* 자꾸 캐시가 남아서 자기 자신을 파괴하는 버그 발생해서 위처럼 수정
     // 인스턴스 접근
     public static GameManager Instance
     {
@@ -28,7 +43,6 @@ public class GameManager : MonoBehaviour
             return _instance;
         }
     }
-
     private void Awake()
     {
         // 인스턴스 중복 방지
@@ -42,6 +56,8 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);  // 이미 인스턴스가 있으면 자신을 파괴
         }
     }
+    */
+
 
     // 몬스터 수가 변경될 때 발생하는 이벤트 (int 파라미터는 새로운 몬스터 수) 추가, 감소 이벤트핸들러 다 붙여줄 수 있게
     public event System.Action<int> MonsterCountChanged;
